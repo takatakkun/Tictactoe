663a4610bf581610b9d604e651cb18463f9c9346
takatakkun <tkttkm18@icloud.com>
2024-10-29 18:44:28 +0900

    色塗り

---
+++ try rubic cube/Assets/Scripts/PaintColor.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintColor : MonoBehaviour
+{
+    public Material paintColor;
+    public Material red;
+    public Material green;
+    public Material blue;
+    public Material orange;
+    public Material white;
+
+    void Update()
+    {
+        // マウスクリックまたはタッチが行われたとき
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            // カメラからクリック位置へのRayを生成
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // RaycastHitオブジェクトでヒット情報を格納
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // ヒットしたオブジェクトがクアッドであるかチェック
+                if (hit.collider.gameObject.CompareTag("Quad"))
+                {
+                    // ヒットしたオブジェクトのRendererを取得してマテリアル変更
+                    Renderer quadRenderer = hit.collider.gameObject.GetComponent<Renderer>();
+                    quadRenderer.GetComponent<Renderer>().material.color = paintColor.color;
+                }
+            }
+        }
+    }
+
+    public void RedTurn()
+    {
+        paintColor = red;
+    }
+
+    public void GreenTurn()
+    {
+        paintColor = green;
+    }
+
+    public void BlueTurn()
+    {
+        paintColor = blue;
+    }
+
+    public void OrangeTurn()
+    {
+        paintColor = orange;
+    }
+
+    public void WhiteTurn()
+    {
+        paintColor = white;
+    }
+
+}
